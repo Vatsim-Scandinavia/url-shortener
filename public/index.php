f6fab8eec54ef7d1dc0bcb6cce33f492c84e369f
Daniel <blt950@users.noreply.github.com>
2025-06-07 20:53:42 +0200

    feat: init redirect

---
+++ public/index.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+// Loading the map of short -> full URLs
+$routes = array_change_key_case(include __DIR__ . '/urls.php', CASE_LOWER);
+
+// Grab the requested "path" (strip leading/trailing slashes) and normalize to lowercase
+$path = strtolower(trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ?? '', '/'));
+
+// Figure out the target URL based on the requested path.
+// Exact matches are preferred, but if not found, we fall back to the "root" URL.
+$defaultTarget = $routes[''] ?? null;
+$target = $routes[$path] ?? $defaultTarget;
+
+// Throw a 500 error if no target is found.
+if (!$target) {
+    http_response_code(500);
+    echo '<h1>Configuration error</h1>';
+    exit;
+}
+
+// Build the redirect URL (add utm_source, respecting any existing ?foo=bar)
+$separator = parse_url($target, PHP_URL_QUERY) ? '&' : '?';
+$redirectUrl = $target . $separator . 'utm_source=vatsca.org';
+
+// Redirect
+header('Location: ' . $redirectUrl, true, 302);
+exit;
---
+++ urls.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+return [
+    // When visiting https://vatsca.org/  (i.e. no path)...
+    ''         => 'https://vatsca.org',
+
+    // When visiting https://vatsca.org/foo etc.
+    'atis'      => 'https://atis.adbj.no/',
+    'booking'   => 'https://booking.vatsim-scandinavia.org',
+    'cdm'       => 'https://cdm.vatsim-scandinavia.org/login',
+    'enbr'      => 'https://wiki.vatsim-scandinavia.org/books/norwegian-airports-charts/page/enbr-bergen-flesland',
+    'encn'      => 'https://wiki.vatsim-scandinavia.org/books/norwegian-airports-charts/page/encn-kristiansand-kjevik',
+    'engm'      => 'https://wiki.vatsim-scandinavia.org/books/norwegian-airports-charts/page/engm-oslo-lufthavn',
+    'engmpush'  => 'https://wiki.vatsim-scandinavia.org/books/norwegian-airports-charts/page/engm-oslo-lufthavn#bkmrk-pushback-maps',
+    'entc'      => 'https://wiki.vatsim-scandinavia.org/books/norwegian-airports-charts/page/entc-tromso-langnes',
+    'ento'      => 'https://wiki.vatsim-scandinavia.org/books/norwegian-airports-charts/page/ento-torp-sandefjord',
+    'enva'      => 'https://wiki.vatsim-scandinavia.org/books/norwegian-airports-charts/page/enva-trondheim-vaernes',
+    'enzv'      => 'https://wiki.vatsim-scandinavia.org/books/norwegian-airports-charts/page/enzv-stavangersola',
+    'esggtaxi'  => 'https://wiki.vatsim-scandinavia.org/books/swedish-airports-charts/page/esgg-goteborglandvetter#bkmrk-taxi',
+    'essataxi'  => 'https://wiki.vatsim-scandinavia.org/books/swedish-airports-charts/page/essa-stockholmarlanda#bkmrk-taxi',
+    'feedback'  => 'https://cc.vatsim-scandinavia.org/feedback',
+    'forum'     => 'https://forum.vatsim-scandinavia.org',
+    'fss'       => 'https://fss.vatsim-scandinavia.org',
+    'norwaypilot' => 'https://wiki.vatsim-scandinavia.org/books/norwegian-airports-charts/page/general-recommendations',
+    'stands'      => 'https://stands.vatsim-scandinavia.org/',
+    'supportatcbook' => 'https://youtube.com/watch?v=q-Y0bnx6Ndw&si=ELBxfEOpqWN-zxwD'
+];
